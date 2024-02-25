f95ca597179d5c9efc3f46c6aea3b4f10dd1b29c
Denis Pauk <pauk.denis@gmail.com>
2024-02-25 12:42:14 +0200

    game: savegame sort fields

--- src/game/savegame/tables/clientfields.h
+++ src/game/savegame/tables/clientfields.h
@@ -26,10 +26,10 @@
  * =======================================================================
  */
 
-{"pers.weapon", CLOFS(pers.weapon), F_ITEM},
-{"pers.lastweapon", CLOFS(pers.lastweapon), F_ITEM},
 {"newweapon", CLOFS(newweapon), F_ITEM},
 {"owned_sphere", CLOFS(owned_sphere), F_EDICT},
-{"resp.coop_respawn.weapon", CLOFS(resp.coop_respawn.weapon), F_ITEM, 0, 3},
+{"pers.lastweapon", CLOFS(pers.lastweapon), F_ITEM},
+{"pers.weapon", CLOFS(pers.weapon), F_ITEM},
 {"resp.coop_respawn.lastweapon", CLOFS(resp.coop_respawn.lastweapon), F_ITEM, 0, 3},
+{"resp.coop_respawn.weapon", CLOFS(resp.coop_respawn.weapon), F_ITEM, 0, 3},
 {NULL, 0, F_INT, 0}
--- src/game/savegame/tables/levelfields.h
+++ src/game/savegame/tables/levelfields.h
@@ -28,9 +28,9 @@
  */
 
 {"changemap", LLOFS(changemap), F_LSTRING},
+{"disguise_violator", LLOFS(disguise_violator), F_EDICT},
 {"sight_client", LLOFS(sight_client), F_EDICT},
 {"sight_entity", LLOFS(sight_entity), F_EDICT},
-{"sound_entity", LLOFS(sound_entity), F_EDICT},
 {"sound2_entity", LLOFS(sound2_entity), F_EDICT},
-{"disguise_violator", LLOFS(disguise_violator), F_EDICT},
+{"sound_entity", LLOFS(sound_entity), F_EDICT},
 {NULL, 0, F_INT}
