6466bdf543e43d10ebd6d25f1494cf35294db076
Denis Pauk <pauk.denis@gmail.com>
2024-12-27 17:32:15 +0200

    count vk calls

--- src/client/refresh/vk/vk_warp.c
+++ src/client/refresh/vk/vk_warp.c
@@ -156,6 +156,7 @@ EmitWaterPolys(msurface_t *fa, image_t *texture, const float *modelMatrix,
 	vkCmdBindVertexBuffers(vk_activeCmdbuffer, 0, 1, &vbo, &vboOffset);
 	vkCmdBindIndexBuffer(vk_activeCmdbuffer, *buffer, dstOffset, VK_INDEX_TYPE_UINT16);
 	vkCmdDrawIndexed(vk_activeCmdbuffer, index_pos, 1, 0, 0, 0);
+	printf("%d: %s\n", drawCalls++, __func__);
 }
 
 void
@@ -261,6 +262,7 @@ R_DrawSkyBox(void)
 
 		vkCmdBindIndexBuffer(vk_activeCmdbuffer, *buffer, dstOffset, VK_INDEX_TYPE_UINT16);
 		vkCmdDrawIndexed(vk_activeCmdbuffer, 6, 1, 0, 0, 0);
+		printf("%d: %s\n", drawCalls++, __func__);
 	}
 }
 
