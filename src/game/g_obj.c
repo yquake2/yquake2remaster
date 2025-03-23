bf783cc6cd6c9843b2977894f51809fd38e84680
Denis Pauk <pauk.denis@gmail.com>
2025-03-23 16:08:12 +0200

    game: reuse SpawnSetAnimGroupFrame

--- src/game/g_obj.c
+++ src/game/g_obj.c
@@ -39,28 +39,7 @@
 void
 object_flame1_think(edict_t *self)
 {
-	int num, i, ofs_frames = 0, num_frames = 1;
-	const dmdxframegroup_t * frames;
-
-	frames = gi.SV_GetFrameGroups(self->s.modelindex, &num);
-	for (i = 0; i < num; i++)
-	{
-		if (!strcmp(frames[i].name, "frame"))
-		{
-			ofs_frames = frames[i].ofs;
-			num_frames = frames[i].num;
-			break;
-		}
-	}
-
-	i = self->s.frame - ofs_frames;
-	if (i < 0)
-	{
-		i = 0;
-	}
-	i++;
-
-	self->s.frame = ofs_frames + i % num_frames;
+	SpawnSetAnimGroupFrame(self, "frame");
 	self->nextthink = level.time + FRAMETIME;
 }
 
@@ -100,7 +79,7 @@ SP_object_flame1(edict_t *self)
 void
 object_big_fire_think(edict_t *self)
 {
-	self->s.frame = (self->s.frame + 1) % 60;
+	SpawnSetAnimGroupFrame(self, "frame");
 	self->nextthink = level.time + FRAMETIME;
 
 	/* add particles */
