4d4b879a336f4d37c48e6ebe569f79dc424708a9
Denis Pauk <pauk.denis@gmail.com>
2025-07-20 15:55:41 +0300

    frames parse

--- src/common/models/models_hlmdl.c
+++ src/common/models/models_hlmdl.c
@@ -53,11 +53,9 @@ Mod_LoadHLMDLAnimGroupList(dmdx_t *pheader, const hlmdl_sequence_t *sequences, i
 		pframegroup[i].ofs = frame_offset;
 		pframegroup[i].num = sequences[i].num_frames;
 
-#if 0
 		Mod_UpdateMinMaxByFrames(pheader,
 			pframegroup[i].ofs, pframegroup[i].ofs + pframegroup[i].num,
 			pframegroup[i].mins, pframegroup[i].maxs);
-#endif
 
 		frame_offset += pframegroup[i].num;
 	}
@@ -76,6 +74,7 @@ Mod_LoadModel_HLMDL(const char *mod_name, const void *buffer, int modfilelen)
 	dmdx_t dmdxheader, *pheader;
 	hlmdl_texture_t *in_skins;
 	dmdxmesh_t *mesh_nodes;
+	const hlmdl_bone_t *bones;
 	void *extradata;
 	const hlmdl_sequence_t *sequences;
 	size_t i, num_tris, framesize;
@@ -200,7 +199,7 @@ Mod_LoadModel_HLMDL(const char *mod_name, const void *buffer, int modfilelen)
 	dmdxheader.num_tris = num_tris;
 	dmdxheader.num_glcmds = 0;
 	dmdxheader.num_imgbit = 0;
-	dmdxheader.num_frames = 0; /* total_frames; */
+	dmdxheader.num_frames = total_frames;
 	dmdxheader.num_animgroup = pinmodel.num_seq;
 
 	Com_DPrintf("%s: %s has %d frames\n",
@@ -224,6 +223,138 @@ Mod_LoadModel_HLMDL(const char *mod_name, const void *buffer, int modfilelen)
 
 		Com_Printf("%s: Skin %s: %d %dx%d\n",
 			__func__, in_skins[i].name, in_skins[i].offset, in_skins[i].width, in_skins[i].height);
+	}
+
+	/* Get bones/sequences */
+	bones = (const hlmdl_bone_t *)((const byte *)buffer + pinmodel.ofs_bones);
+	int frame_idx = 0;
+	for (i = 0; i < pinmodel.num_seq; i++)
+	{
+		const hlmdl_sequence_t *seq = &sequences[i];
+		for (int f = 0; f < seq->num_frames; f++, frame_idx++)
+		{
+			daliasxframe_t *frame = (daliasxframe_t *)((byte *)pheader +
+				pheader->ofs_frames + frame_idx * pheader->framesize);
+
+			/* Name frame as sequenceName_frameIdx */
+			snprintf(frame->name, sizeof(frame->name), "%s_%d", seq->name, f);
+
+			Com_Printf("%s: %s, Sequence '%s': frame %d/%d\n",
+				__func__, mod_name, seq->name, f, seq->num_frames);
+
+			/* Use standard scale/translation for now */
+			VectorSet(frame->scale, 1.0f, 1.0f, 1.0f);
+			VectorSet(frame->translate, 0.f, 0.f, 0.f);
+
+			/* Convert bone positions to vertices (same as before) */
+			for (int j = 0; j < pinmodel.num_bones; j++)
+			{
+				dxtrivertx_t *vert = &frame->verts[j];
+				const hlmdl_bone_t *bone = &bones[j];
+
+				vec3_t pos = {0};
+				vec3_t rotation = {0};
+				vec3_t final_pos = {0};
+
+				for (int k = 0; k < 3; k++)
+				{
+					pos[k] = bone->value[k];
+					rotation[k] = bone->value[k + 3];
+					if (bone->scale[k] != 0)
+						pos[k] *= bone->scale[k];
+					if (bone->scale[k + 3] != 0)
+						rotation[k] *= bone->scale[k + 3];
+				}
+
+				if (bone->parent >= 0)
+				{
+					vec3_t parent_pos = {0};
+					const hlmdl_bone_t *parent = &bones[bone->parent];
+					for (int k = 0; k < 3; k++)
+					{
+						parent_pos[k] = parent->value[k];
+						if (parent->scale[k] != 0)
+						{
+							parent_pos[k] *= parent->scale[k];
+						}
+					}
+
+					float cr = cos(parent->value[3]);
+					float sr = sin(parent->value[3]);
+					float cp = cos(parent->value[4]);
+					float sp = sin(parent->value[4]);
+					float cy = cos(parent->value[5]);
+					float sy = sin(parent->value[5]);
+
+					vec3_t rotated_pos;
+
+					/* Rotate around X */
+					rotated_pos[0] = pos[0];
+					rotated_pos[1] = pos[1] * cr - pos[2] * sr;
+					rotated_pos[2] = pos[1] * sr + pos[2] * cr;
+
+					/* Rotate around Y */
+					pos[0] = rotated_pos[0] * cp + rotated_pos[2] * sp;
+					pos[1] = rotated_pos[1];
+					pos[2] = -rotated_pos[0] * sp + rotated_pos[2] * cp;
+
+					/* Rotate around Z */
+					rotated_pos[0] = pos[0] * cy - pos[1] * sy;
+					rotated_pos[1] = pos[0] * sy + pos[1] * cy;
+					rotated_pos[2] = pos[2];
+
+					/* Add parent position */
+					VectorAdd(rotated_pos, parent_pos, final_pos);
+				}
+				else
+				{
+					VectorCopy(pos, final_pos);
+				}
+
+				/* Convert to normalized 8-bit vertex coords */
+				float scale = 64.0f; /* Scale factor to fit model size */
+				for (int k = 0; k < 3; k++)
+				{
+					float normalized = (final_pos[k] / scale + 1.0f) * 127.5f;
+					vert->v[k] = (byte)normalized;
+				}
+
+				/* Calculate vertex normal from bone rotation */
+				float cr = cos(rotation[0]);
+				float sr = sin(rotation[0]);
+				float cp = cos(rotation[1]);
+				float sp = sin(rotation[1]);
+				float cy = cos(rotation[2]);
+				float sy = sin(rotation[2]);
+
+				/* Default normal points up (0,0,1) transformed by bone rotation */
+				vec3_t normal = {0, 0, 1};
+				vec3_t temp;
+
+				/* Apply rotations in order: Y, P, R */
+				/* Yaw */
+				temp[0] = normal[0] * cy - normal[1] * sy;
+				temp[1] = normal[0] * sy + normal[1] * cy;
+				temp[2] = normal[2];
+
+				/* Pitch */
+				normal[0] = temp[0] * cp + temp[2] * sp;
+				normal[1] = temp[1];
+				normal[2] = -temp[0] * sp + temp[2] * cp;
+
+				/* Roll */
+				temp[0] = normal[0];
+				temp[1] = normal[1] * cr - normal[2] * sr;
+				temp[2] = normal[1] * sr + normal[2] * cr;
+
+				/* Store normalized normal vector */
+				VectorNormalize(temp);
+				for (int k = 0; k < 3; k++)
+				{
+					vert->normal[k] = (byte)((temp[k] + 1.0f) * 127.5f);
+				}
+			}
+		}
 	}
 
 	Mod_LoadHLMDLAnimGroupList(pheader, sequences, pinmodel.num_seq);
