00804217d19358e8725c5b1dc8f790cdebb88b0e
Denis Pauk <pauk.denis@gmail.com>
2026-06-15 23:43:50 +0300

    game: add small distance from solid in FixEntityPosition
    Based on:
     * https://github.com/id-Software/quake2-rerelease-dll

--- src/game/g_items.c
+++ src/game/g_items.c
@@ -1983,6 +1983,7 @@ GetBetterPosition(const vec3_t ent_mins, const vec3_t ent_maxs, const edict_t *p
 				if (best_dist > diff_pos_len)
 				{
 					VectorCopy(tr_pos.endpos, best_pos);
+					best_pos[(j % 3)] += 0.125f;
 					best_dist = diff_pos_len;
 				}
 			}
@@ -2007,6 +2008,7 @@ GetBetterPosition(const vec3_t ent_mins, const vec3_t ent_maxs, const edict_t *p
 				if (best_dist > diff_pos_len)
 				{
 					VectorCopy(tr_pos.endpos, best_pos);
+					best_pos[(j % 3)] -= 0.125f;
 					best_dist = diff_pos_len;
 				}
 			}
