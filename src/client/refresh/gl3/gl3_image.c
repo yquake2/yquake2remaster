922285e16961c6d5bedc9d7504bb3a3498b8eebd
Daniel Gibson <metalcaedes@gmail.com>
2026-07-15 00:08:20 +0200

    GL3: Port scrap texture from GL1
    so small 2D (UI) textures are merged into one texture, which allows
    reducing draw calls even further (theoretically all HUD drawcalls can
    be merged into one now, in practice it seems to be 2 or 3 - good enough)
    
    OpenGL3 guarantees that 1024x1024 textures are supported (GLES3 even
    2048x2048) so I increased the scrap size to 1024x1024 and increased the
    limit for "small" textures that go there to 128x128

--- src/client/refresh/gl3/gl3_image.c
+++ src/client/refresh/gl3/gl3_image.c
@@ -81,11 +81,15 @@ GL3_Scrap_Upload(void)
 			   linear for others */
 			if (i == 0)
 			{
+				// 2D textures shouldn't be filtered by default (r_2D_unfiltered),
+				// so the scrap shouldn't be filtered
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			}
-			else
+			else // 2D textures should be filtered by default => filter the scrap
 			{
+				// we can't use gl_filter_min which might be GL_*_MIPMAP_*
+				// also, there's no anisotropic filtering for textures w/o mipmaps
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, gl_filter_max);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, gl_filter_max);
 			}
@@ -436,7 +440,8 @@ GL3_LoadPic(const char *name, byte *pic, int width, int realwidth,
 	GLuint texNum=0;
 	int i;
 
-	if (r_2D_unfiltered->value && type == it_pic)
+	qboolean default2Dnolerp = r_2D_unfiltered->value != 0.0f;
+	if (default2Dnolerp && type == it_pic)
 	{
 		/*
 		 * if r_2D_unfiltered is true(ish), nolerp should usually be true,
@@ -537,7 +542,7 @@ GL3_LoadPic(const char *name, byte *pic, int width, int realwidth,
 
 		if (bits == 32)
 		{
-			texnum = Scrap_AllocBlock(width, height, &x, &y, (unsigned*)pic, nolerp ? 0 : 1);
+			texnum = Scrap_AllocBlock(width, height, &x, &y, (unsigned*)pic, (nolerp || default2Dnolerp) ? 0 : 1);
 		}
 		else
 		{
@@ -546,7 +551,7 @@ GL3_LoadPic(const char *name, byte *pic, int width, int realwidth,
 			trans = R_Convert8to32(pic, width, height, d_8to24table);
 			if (trans)
 			{
-				texnum = Scrap_AllocBlock(width, height, &x, &y, trans, nolerp ? 0 : 1);
+				texnum = Scrap_AllocBlock(width, height, &x, &y, trans, (nolerp || default2Dnolerp) ? 0 : 1);
 				free(trans);
 			}
 		}
