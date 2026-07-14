c4388291b22df0d1abd5bc63b940dd2037468a9d
Daniel Gibson <metalcaedes@gmail.com>
2026-07-14 20:51:38 +0200

    Enforce drawing batched 2D draws before drawing other stuff
    to preserve draw order. fixes the playermodel preview in the multiplayer
    player setup menu and possibly other stuff

--- src/client/refresh/gl3/header/local.h
+++ src/client/refresh/gl3/header/local.h
@@ -428,6 +428,7 @@ extern void GL3_Draw_Flash(const float color[4], float x, float y, float w, floa
 extern void GL3_Draw_StretchRaw(int x, int y, int w, int h, int cols, int rows, const byte *data, int bits);
 extern void GL3_Draw_FreeLocal(void);
 
+extern void GL3_DrawCurrent2Dbatch();
 extern void GL3_EndFrame(void);
 
 // gl3_image.c
