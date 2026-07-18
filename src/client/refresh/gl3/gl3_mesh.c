yquake2/yquake2

Batch 3D drawcalls (except for MD2 models)

src/client/refresh/gl3/gl3_mesh.c
@@ -849,7 +849,7 @@ GL3_DrawAliasModel(entity_t *entity)
 	origModelMat = gl3state.uni3DData.transModelMat4;
 
 	entity->angles[PITCH] = -entity->angles[PITCH];
-	GL3_RotateForEntity(entity);
+	GL3_RotateUni3DforEntity(entity);
 	entity->angles[PITCH] = -entity->angles[PITCH];
 
 
@@ -908,17 +908,16 @@ GL3_DrawAliasModel(entity_t *entity)
 
 	DrawAliasFrameLerp(paliashdr, entity, shadelight);
 
-	//glPopMatrix();
-	gl3state.uni3DData.transModelMat4 = origModelMat;
-	GL3_UpdateUBO3D();
-
 	if (entity->flags & RF_WEAPONMODEL)
 	{
 		gl3state.uni3DData.transProjViewMat4 = origProjViewMat;
-		GL3_UpdateUBO3D();
 		if(gl_lefthand->value == 1.0F)
 			glCullFace(GL_FRONT);
 	}
+
+	//glPopMatrix();
+	gl3state.uni3DData.transModelMat4 = origModelMat;
+	GL3_UpdateUBO3D();
 
 	if (entity->flags & RF_TRANSLUCENT)
 	{

src/client/refresh/gl3/gl3_warp.c
@@ -228,7 +228,7 @@ GL3_SubdivideSurface(msurface_t *fa, gl3model_t* loadmodel)
  * Does a water warp on the pre-fragmented glpoly_t chain
  */
 void
-GL3_EmitWaterPolys(msurface_t *fa)
+GL3_EmitWaterPolys(msurface_t *fa, gl3drawCmd_t drawCmd)
 {
 	glpoly_t *bp;
 	float scroll = 0.0f;
@@ -241,37 +241,25 @@ GL3_EmitWaterPolys(msurface_t *fa)
 			scroll = -64.0f;
 		}
 	}
+	// set this even if scroll is 0, because the shader uses scroll
+	// so the uniform must be set either way
+	drawCmd.scroll = scroll;
+	drawCmd.flags |= DCFlag_UseScroll;
 
-	qboolean updateUni3D = false;
-	if(gl3state.uni3DData.scroll != scroll)
-	{
-		gl3state.uni3DData.scroll = scroll;
-		updateUni3D = true;
-	}
 	// these surfaces (mostly water and lava, I think?) don't have a lightmap.
 	// rendering water at full brightness looks bad (esp. for water in dark environments)
 	// so default use a factor of 0.5 (ontop of intensity)
 	// but lava should be bright and glowing, so use full brightness there
-	float lightScale = fa->texinfo->image->is_lava ? 1.0f : 0.5f;
-	if(lightScale != gl3state.uni3DData.lightScaleForTurb)
-	{
-		gl3state.uni3DData.lightScaleForTurb = lightScale;
-		updateUni3D = true;
-	}
+	drawCmd.lightScaleForTurb = fa->texinfo->image->is_lava ? 1.0f : 0.5f;
+	drawCmd.flags |= DCFlag_UseLightScaleForTurb;
 
-	if(updateUni3D)
-	{
-		GL3_UpdateUBO3D();
-	}
 
-	GL3_UseProgram(gl3state.si3Dturb.shaderProgram);
 
-	GL3_BindVAO(gl3state.vao3D);
-	GL3_BindVBO(gl3state.vbo3D);
+	drawCmd.shader = &gl3state.si3Dturb;
 
 	for (bp = fa->polys; bp != NULL; bp = bp->next)
 	{
-		GL3_BufferAndDraw3D(bp->vertices, bp->numverts, GL_TRIANGLE_FAN);
+		GL3_BufferAndDraw3D(bp->vertices, bp->numverts, GL_TRIANGLE_FAN, drawCmd);
 	}
 }
 
@@ -696,26 +684,25 @@ GL3_DrawSkyBox(void)
 		}
 	}
 
+	gl3drawCmd_t drawCmd = GL3_CreateDrawCmd(false);
+
 	// glPushMatrix();
-	hmm_mat4 origModelMat = gl3state.uni3DData.transModelMat4;
 
 	// glTranslatef(gl3_origin[0], gl3_origin[1], gl3_origin[2]);
 	hmm_vec3 transl = HMM_Vec3(gl3_origin[0], gl3_origin[1], gl3_origin[2]);
-	hmm_mat4 modMVmat = HMM_MultiplyMat4(origModelMat, HMM_Translate(transl));
+	hmm_mat4 modMVmat = HMM_Translate(transl);
 	if(skyrotate != 0.0f)
 	{
 		// glRotatef(r_newrefdef.time * skyrotate, skyaxis[0], skyaxis[1], skyaxis[2]);
 		hmm_vec3 rotAxis = HMM_Vec3(skyaxis[0], skyaxis[1], skyaxis[2]);
 		modMVmat = HMM_MultiplyMat4(modMVmat, HMM_Rotate(r_newrefdef.time * skyrotate, rotAxis));
 	}
-	gl3state.uni3DData.transModelMat4 = modMVmat;
-	GL3_UpdateUBO3D();
+	drawCmd.transModelMat = modMVmat;
 
-	GL3_UseProgram(gl3state.si3Dsky.shaderProgram);
-	GL3_BindVAO(gl3state.vao3D);
-	GL3_BindVBO(gl3state.vbo3D);
+	drawCmd.shader = &gl3state.si3Dsky;
 
 	// TODO: this could all be done in one drawcall.. but.. whatever, it's <= 6 drawcalls/frame
+	//       also, they use different textures..
 
 	gl3_3D_vtx_t skyVertices[4];
 
@@ -735,17 +722,15 @@ GL3_DrawSkyBox(void)
 			continue;
 		}
 
-		GL3_Bind(sky_images[skytexorder[i]]->texnum);
+		drawCmd.texnum = sky_images[skytexorder[i]]->texnum;
 
 		MakeSkyVec( skymins [ 0 ] [ i ], skymins [ 1 ] [ i ], i, &skyVertices[0] );
 		MakeSkyVec( skymins [ 0 ] [ i ], skymaxs [ 1 ] [ i ], i, &skyVertices[1] );
 		MakeSkyVec( skymaxs [ 0 ] [ i ], skymaxs [ 1 ] [ i ], i, &skyVertices[2] );
 		MakeSkyVec( skymaxs [ 0 ] [ i ], skymins [ 1 ] [ i ], i, &skyVertices[3] );
 
-		GL3_BufferAndDraw3D(skyVertices, 4, GL_TRIANGLE_FAN);
+		GL3_BufferAndDraw3D(skyVertices, 4, GL_TRIANGLE_FAN, drawCmd);
 	}
 
 	// glPopMatrix();
-	gl3state.uni3DData.transModelMat4 = origModelMat;
-	GL3_UpdateUBO3D();
 }

src/client/refresh/gl3/header/local.h
@@ -254,11 +254,9 @@ typedef struct
 	// NOTE: make sure siParticle is always the last shaderInfo (or adapt GL3_ShutdownShaders())
 	gl3ShaderInfo_t siParticle; // for particles. surprising, right?
 
-	GLuint vao3D, vbo3D; // for brushes etc, using 10 floats and one uint as vertex input (x,y,z, s,t, lms,lmt, normX,normY,normZ ; lightFlags)
+	GLuint vao3D, vbo3D, ebo3D; // for brushes etc, using 10 floats and one uint as vertex input (x,y,z, s,t, lms,lmt, normX,normY,normZ ; lightFlags)
 
 	// the next two are for gl3config.useBigVBO == true
-	int vbo3Dsize;
-	int vbo3DcurOffset;
 
 	GLuint vaoAlias, vboAlias, eboAlias; // for models, using 9 floats as (x,y,z, s,t, r,g,b,a)
 	GLuint vaoParticle, vboParticle; // for particles, using 9 floats (x,y,z, size,distance, r,g,b,a)
@@ -276,6 +274,60 @@ typedef struct
 	hmm_mat4 projMat3D;
 	hmm_mat4 viewMat3D;
 } gl3state_t;
+
+
+// drawcommands using gl3_3D_vtx_t, for batching
+typedef struct gl3drawCmd_s {
+	hmm_mat4	transModelMat;
+	gl3ShaderInfo_t* shader;
+
+	GLuint		texnum;
+	int			lmtexnum;
+
+	float		scroll; // for gl3state.uni3DData.scroll
+	float		lightScaleForTurb; // for gl3state.uni3DData.lightScaleForTurb
+	float		alpha; // either part of color or for gl3state.uni3DData.alpha
+	byte		color[3]; // for uniCommonData.color; its alpha chan is in .alpha
+	byte		flags;    // gl3drawCmd_Flags
+	byte		styles[MAXLIGHTMAPS]; // indexes into r_newrefdef.lightstyles[]; 255 means "ignore"
+
+	// the following are set in GL3_BufferAndDraw3D()
+	int			idxBufOffset;
+	int			numElements; // in index buffer
+} gl3drawCmd_t;
+
+// for gl3drawCmd_t::flags
+enum gl3drawCmd_Flags {
+	DCFlag_DisableDepthMask =  1, // glDepthMask() - GL_FALSE if bit is set
+	DCFlag_Blend            =  2, // GL_BLEND (glEnable/glDisable)
+	DCFlag_PolyOffsetFill   =  4, // GL_POLYGON_OFFSET_FILL (glEnable/glDisable) - for gl_zfix
+	DCFlag_UseColor         =  8,
+	DCFlag_UseScroll        = 16,
+	DCFlag_UseLmStyles      = 32,
+	DCFlag_UseLightScaleForTurb = 64,
+	DCFlag_IsIdentityMat    = 128, // avoids comparing the matrix in many cases
+
+	// TODO: DCFlag_SameAsPrevious = 255 for "don't check, just merge into previous command"?
+};
+
+// create an "empty" gl3drawCmd_t with sane defaults
+static inline gl3drawCmd_t
+GL3_CreateDrawCmd(qboolean identityTrans)
+{
+	gl3drawCmd_t ret = {0};
+	if(identityTrans) {
+		ret.transModelMat = gl3_identityMat4;
+		ret.flags = DCFlag_IsIdentityMat;
+	}
+	ret.alpha = 1.0f;
+	ret.styles[0] = 255;
+	ret.lmtexnum = -1;
+	// the other values can remain 0/NULL
+
+	return ret;
+}
+
+
 
 extern gl3config_t gl3config;
 extern gl3state_t gl3state;
@@ -382,9 +434,11 @@ GL3_BindEBO(GLuint ebo)
 	}
 }
 
-extern void GL3_BufferAndDraw3D(const gl3_3D_vtx_t* verts, int numVerts, GLenum drawMode);
+extern void GL3_BufferAndDraw3D(const gl3_3D_vtx_t* verts, int numVerts, GLenum drawMode, gl3drawCmd_t drawCmd);
+extern void GL3_Draw3DBatchesNow(void);
 
-extern void GL3_RotateForEntity(entity_t *e);
+extern void GL3_RotateUni3DforEntity(entity_t *e);
+extern void GL3_RotateForEntity(entity_t *e, gl3drawCmd_t* drawCmd, qboolean replaceTransModelMat);
 
 // gl3_sdl.c
 extern int GL3_InitContext(void* win);
@@ -480,7 +534,7 @@ extern void GL3_LM_BeginBuildingLightmaps(gl3model_t *m);
 extern void GL3_LM_EndBuildingLightmaps(void);
 
 // gl3_warp.c
-extern void GL3_EmitWaterPolys(msurface_t *fa);
+extern void GL3_EmitWaterPolys(msurface_t *fa, gl3drawCmd_t drawCmd);
 extern void GL3_SubdivideSurface(msurface_t *fa, gl3model_t* loadmodel);
 
 extern void GL3_SetSky(const char *name, float rotate, vec3_t axis);
@@ -492,8 +546,8 @@ extern void GL3_AddSkySurface(msurface_t *fa);
 // gl3_surf.c
 extern void GL3_SurfInit(void);
 extern void GL3_SurfShutdown(void);
-extern void GL3_DrawGLPoly(msurface_t *fa);
-extern void GL3_DrawGLFlowingPoly(msurface_t *fa);
+extern void GL3_DrawGLPoly(msurface_t *fa, gl3drawCmd_t drawCmd);
+extern void GL3_DrawGLFlowingPoly(msurface_t *fa, gl3drawCmd_t drawCmd);
 extern void GL3_DrawTriangleOutlines(void);
 extern void GL3_DrawAlphaSurfaces(void);
 extern void GL3_DrawBrushModel(entity_t *e, gl3model_t *currentmodel);

yquake2/yquake2remaster

src/client/refresh/gl3/gl3_mesh.c
@@ -560,7 +560,7 @@ GL3_DrawAliasModel(entity_t *currententity)
 	origModelMat = gl3state.uni3DData.transModelMat4;
 
 	currententity->angles[PITCH] = -currententity->angles[PITCH];
-	GL3_RotateForEntity(currententity);
+	GL3_RotateUni3DforEntity(currententity);
 	currententity->angles[PITCH] = -currententity->angles[PITCH];
 
 	/* select skin */
@@ -613,19 +613,18 @@ GL3_DrawAliasModel(entity_t *currententity)
 
 	DrawAliasFrameLerp(paliashdr, currententity, shadelight, shadevector);
 
-	//glPopMatrix();
-	gl3state.uni3DData.transModelMat4 = origModelMat;
-	GL3_UpdateUBO3D();
-
 	if (currententity->flags & RF_WEAPONMODEL)
 	{
 		gl3state.uni3DData.transProjViewMat4 = origProjViewMat;
-		GL3_UpdateUBO3D();
 		if (r_lefthand->value == 1.0F)
 		{
 			glCullFace(GL_FRONT);
 		}
 	}
+
+	//glPopMatrix();
+	gl3state.uni3DData.transModelMat4 = origModelMat;
+	GL3_UpdateUBO3D();
 
 	if (currententity->flags & RF_TRANSLUCENT)
 	{

src/client/refresh/gl3/gl3_warp.c
@@ -31,44 +31,32 @@
  * Does a water warp on the pre-fragmented mpoly_t chain
  */
 void
-GL3_EmitWaterPolys(msurface_t *fa)
+GL3_EmitWaterPolys(msurface_t *fa, gl3drawCmd_t drawCmd)
 {
 	const mpoly_t *bp;
 	float sscroll, tscroll = 0.0f;
 
 	R_FlowingScroll(&r_newrefdef, fa->texinfo->flags, &sscroll, &tscroll);
 
-	qboolean updateUni3D = false;
-	if ((gl3state.uni3DData.sscroll != sscroll) || (gl3state.uni3DData.tscroll != tscroll))
-	{
-		gl3state.uni3DData.sscroll = sscroll;
-		gl3state.uni3DData.tscroll = tscroll;
-		updateUni3D = true;
-	}
+	// set this even if scroll is 0, because the shader uses scroll
+	// so the uniform must be set either way
+	drawCmd.sscroll = sscroll;
+	drawCmd.tscroll = tscroll;
+	drawCmd.flags |= DCFlag_UseScroll;
+
 	// these surfaces (mostly water and lava, I think?) don't have a lightmap.
 	// rendering water at full brightness looks bad (esp. for water in dark environments)
 	// so default use a factor of 0.5 (ontop of intensity)
 	// but lava should be bright and glowing, so use full brightness there
-	float lightScale = fa->texinfo->image->is_lava ? 1.0f : 0.5f;
-	if (lightScale != gl3state.uni3DData.lightScaleForTurb)
-	{
-		gl3state.uni3DData.lightScaleForTurb = lightScale;
-		updateUni3D = true;
-	}
 
-	if (updateUni3D)
-	{
-		GL3_UpdateUBO3D();
-	}
+	drawCmd.lightScaleForTurb = fa->texinfo->image->is_lava ? 1.0f : 0.5f;
+	drawCmd.flags |= DCFlag_UseLightScaleForTurb;
 
-	GL3_UseProgram(gl3state.si3Dturb.shaderProgram);
-
-	GL3_BindVAO(gl3state.vao3D);
-	GL3_BindVBO(gl3state.vbo3D);
+	drawCmd.shader = &gl3state.si3Dturb;
 
 	for (bp = fa->polys; bp != NULL; bp = bp->next)
 	{
-		GL3_BufferAndDraw3D(bp->verts, bp->numverts, GL_TRIANGLE_FAN);
+		GL3_BufferAndDraw3D(bp->verts, bp->numverts, GL_TRIANGLE_FAN, drawCmd);
 	}
 }
 
@@ -155,12 +143,13 @@ GL3_DrawSkyBox(void)
 		}
 	}
 
+	gl3drawCmd_t drawCmd = GL3_CreateDrawCmd(false);
+
 	// glPushMatrix();
-	hmm_mat4 origModelMat = gl3state.uni3DData.transModelMat4;
 
 	// glTranslatef(gl3_origin[0], gl3_origin[1], gl3_origin[2]);
 	hmm_vec3 transl = HMM_Vec3(gl3_origin[0], gl3_origin[1], gl3_origin[2]);
-	hmm_mat4 modMVmat = HMM_MultiplyMat4(origModelMat, HMM_Translate(transl));
+	hmm_mat4 modMVmat = HMM_Translate(transl);
 	if (skyrotate != 0.0f)
 	{
 		// glRotatef(r_newrefdef.time * skyrotate, skyaxis[0], skyaxis[1], skyaxis[2]);
@@ -168,14 +157,12 @@ GL3_DrawSkyBox(void)
 		modMVmat = HMM_MultiplyMat4(modMVmat, HMM_Rotate(
 			(skyautorotate ? r_newrefdef.time : 1.f) * skyrotate, rotAxis));
 	}
-	gl3state.uni3DData.transModelMat4 = modMVmat;
-	GL3_UpdateUBO3D();
+	drawCmd.transModelMat = modMVmat;
 
-	GL3_UseProgram(gl3state.si3Dsky.shaderProgram);
-	GL3_BindVAO(gl3state.vao3D);
-	GL3_BindVBO(gl3state.vbo3D);
+	drawCmd.shader = &gl3state.si3Dsky;
 
 	// TODO: this could all be done in one drawcall.. but.. whatever, it's <= 6 drawcalls/frame
+	//       also, they use different textures..
 
 	mvtx_t skyVertices[4];
 
@@ -195,7 +182,7 @@ GL3_DrawSkyBox(void)
 			continue;
 		}
 
-		GL3_Bind(sky_images[skytexorder[i]]->texnum);
+		drawCmd.texnum = sky_images[skytexorder[i]]->texnum;
 
 		R_MakeSkyVec( skymins [ 0 ] [ i ], skymins [ 1 ] [ i ], i, &skyVertices[0],
 			r_worldmodel, sky_min, sky_max);
@@ -206,10 +193,8 @@ GL3_DrawSkyBox(void)
 		R_MakeSkyVec( skymaxs [ 0 ] [ i ], skymins [ 1 ] [ i ], i, &skyVertices[3],
 			r_worldmodel, sky_min, sky_max);
 
-		GL3_BufferAndDraw3D(skyVertices, 4, GL_TRIANGLE_FAN);
+		GL3_BufferAndDraw3D(skyVertices, 4, GL_TRIANGLE_FAN, drawCmd);
 	}
 
 	// glPopMatrix();
-	gl3state.uni3DData.transModelMat4 = origModelMat;
-	GL3_UpdateUBO3D();
 }

src/client/refresh/gl3/header/local.h
@@ -255,11 +255,9 @@ typedef struct
 	// NOTE: make sure siParticle is always the last shaderInfo (or adapt GL3_ShutdownShaders())
 	gl3ShaderInfo_t siParticle; // for particles. surprising, right?
 
-	GLuint vao3D, vbo3D; // for brushes etc, using 10 floats and one uint as vertex input (x,y,z, s,t, lms,lmt, normX,normY,normZ ; lightFlags)
+	GLuint vao3D, vbo3D, ebo3D; // for brushes etc, using 10 floats and one uint as vertex input (x,y,z, s,t, lms,lmt, normX,normY,normZ ; lightFlags)
 
 	// the next two are for gl3config.useBigVBO == true
-	int vbo3Dsize;
-	int vbo3DcurOffset;
 
 	GLuint vaoAlias, vboAlias, eboAlias; // for models, using 9 floats as (x,y,z, s,t, r,g,b,a)
 	GLuint vaoParticle, vboParticle; // for particles, using 9 floats (x,y,z, size,distance, r,g,b,a)
@@ -277,6 +275,62 @@ typedef struct
 	hmm_mat4 projMat3D;
 	hmm_mat4 viewMat3D;
 } gl3state_t;
+
+
+// drawcommands using gl3_3D_vtx_t, for batching
+typedef struct gl3drawCmd_s {
+	hmm_mat4	transModelMat;
+	gl3ShaderInfo_t* shader;
+
+	GLuint		texnum;
+	int			lmtexnum;
+
+	float		sscroll; // for gl3state.uni3DData.scroll
+	float		tscroll; // for gl3state.uni3DData.scroll
+	float		lightScaleForTurb; // for gl3state.uni3DData.lightScaleForTurb
+	float		alpha; // either part of color or for gl3state.uni3DData.alpha
+	byte		color[3]; // for uniCommonData.color; its alpha chan is in .alpha
+	int			flags;    // gl3drawCmd_Flags
+	byte		styles[MAXLIGHTMAPS]; // indexes into r_newrefdef.lightstyles[]; 255 means "ignore"
+
+	// the following are set in GL3_BufferAndDraw3D()
+	int			idxBufOffset;
+	int			numElements; // in index buffer
+} gl3drawCmd_t;
+
+// for gl3drawCmd_t::flags
+enum gl3drawCmd_Flags {
+	DCFlag_DisableDepthMask =  1, // glDepthMask() - GL_FALSE if bit is set
+	DCFlag_Blend            =  2, // GL_BLEND (glEnable/glDisable)
+	DCFlag_PolyOffsetFill   =  4, // GL_POLYGON_OFFSET_FILL (glEnable/glDisable) - for gl_zfix
+	DCFlag_UseColor         =  8,
+	DCFlag_UseScroll        = 16,
+	DCFlag_UseLmStyles      = 32,
+	DCFlag_UseLightScaleForTurb = 64,
+	DCFlag_IsIdentityMat    = 128, // avoids comparing the matrix in many cases
+	DCFlag_Flare            = 256, // Combine flare effect
+
+	// TODO: DCFlag_SameAsPrevious = 255 for "don't check, just merge into previous command"?
+};
+
+// create an "empty" gl3drawCmd_t with sane defaults
+static inline gl3drawCmd_t
+GL3_CreateDrawCmd(qboolean identityTrans)
+{
+	gl3drawCmd_t ret = {0};
+	if(identityTrans) {
+		ret.transModelMat = gl3_identityMat4;
+		ret.flags = DCFlag_IsIdentityMat;
+	}
+	ret.alpha = 1.0f;
+	ret.styles[0] = 255;
+	ret.lmtexnum = -1;
+	// the other values can remain 0/NULL
+
+	return ret;
+}
+
+
 
 extern gl3config_t gl3config;
 extern gl3state_t gl3state;
@@ -380,8 +434,10 @@ GL3_BindEBO(GLuint ebo)
 	}
 }
 
-extern void GL3_BufferAndDraw3D(const mvtx_t* verts, int numVerts, GLenum drawMode);
-extern void GL3_RotateForEntity(entity_t *e);
+extern void GL3_BufferAndDraw3D(const mvtx_t* verts, int numVerts, GLenum drawMode, gl3drawCmd_t drawCmd);
+extern void GL3_Draw3DBatchesNow(void);
+extern void GL3_RotateUni3DforEntity(entity_t *e);
+extern void GL3_RotateForEntity(entity_t *e, gl3drawCmd_t* drawCmd, qboolean replaceTransModelMat);
 extern hmm_mat4 GL3_SetPerspective(GLdouble fovy);
 
 // gl3_sdl.c
@@ -473,7 +529,7 @@ extern void LM_BeginBuildingLightmaps(model_t *m);
 extern void LM_EndBuildingLightmaps(void);
 
 // gl3_warp.c
-extern void GL3_EmitWaterPolys(msurface_t *fa);
+extern void GL3_EmitWaterPolys(msurface_t *fa, gl3drawCmd_t drawCmd);
 
 extern void GL3_SetSky(const char *name, float rotate, int autorotate, const vec3_t axis);
 extern void GL3_DrawSkyBox(void);